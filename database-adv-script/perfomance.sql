0f2d1d6585bc6771fe3ccf50a29c98e7b2cd3e3d
nuhamintesfe <nuhamintesfaye026@gmail.com>
2025-05-15 15:26:23 +0300

    Create perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,6 @@
+-- Initial complex query with multiple joins
+SELECT b.id AS booking_id, u.name AS user_name, p.name AS property_name, pay.amount
+FROM bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+JOIN payments pay ON b.id = pay.booking_id;
