df50c8369c1e93af1d5b794fdc3545a10d82191a
nuhamintesfe <nuhamintesfaye026@gmail.com>
2025-05-15 14:49:35 +0300

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,20 @@
+-- INNER JOIN: Bookings with Users
+SELECT b.*, u.*
+FROM bookings b
+INNER JOIN users u ON b.user_id = u.id;
+
+-- LEFT JOIN: Properties with their Reviews (include properties without reviews)
+SELECT p.*, r.*
+FROM properties p
+LEFT JOIN reviews r ON p.id = r.property_id;
+
+-- FULL OUTER JOIN workaround using UNION of LEFT and RIGHT JOIN
+SELECT u.*, b.*
+FROM users u
+LEFT JOIN bookings b ON u.id = b.user_id
+
+UNION
+
+SELECT u.*, b.*
+FROM users u
+RIGHT JOIN bookings b ON u.id = b.user_id;
