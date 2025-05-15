5e0b5f86b9a121d94c90f2b2b3f0fe93e129b138
nuhamintesfe <nuhamintesfaye026@gmail.com>
2025-05-15 15:15:41 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,12 +1,14 @@
 -- INNER JOIN: Bookings with Users
 SELECT b.*, u.*
 FROM bookings b
-INNER JOIN users u ON b.user_id = u.id;
+INNER JOIN users u ON b.user_id = u.id
+ORDER BY b.id;
 
 -- LEFT JOIN: Properties with their Reviews (include properties without reviews)
 SELECT p.*, r.*
 FROM properties p
-LEFT JOIN reviews r ON p.id = r.property_id;
+LEFT JOIN reviews r ON p.id = r.property_id
+ORDER BY p.id;
 
 -- FULL OUTER JOIN workaround using UNION of LEFT and RIGHT JOIN
 SELECT u.*, b.*
@@ -17,4 +19,6 @@ UNION
 
 SELECT u.*, b.*
 FROM users u
-RIGHT JOIN bookings b ON u.id = b.user_id;
+RIGHT JOIN bookings b ON u.id = b.user_id
+ORDER BY user_id, booking_id;
+
